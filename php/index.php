245756a6701cbc01a43e4ba5bcd41a64c351b877
Gaurav-Janjvadiya <janjvadiyagaurav344@gmail.com>
2024-02-13 19:33:36 +0530

    commit

--- php/index.php
+++ php/index.php
@@ -1,34 +1,50 @@
+<!DOCTYPE html>
 <html>
-<head></head>
-<body>
-<form method="POST">
-<center>
-username:<input type="text" name="username" required/><br>
-subject:<input type="number" name="subject1" required>
-        <input type="number" name="subject2" required>
-        <input type="number" name="subject3" required>
-        <input type="submit" name="submit" value="submit" required>
-</center>
-</form>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body 
+style="height: 100vh; background-color: lightgray; width: 100%; display: flex; justify-content: center ; align-items: center;  letter-spacing: 2px;"
+>
+    <form 
+    style="flex-direction: column; display: flex; padding:1rem; border: 1px solid gray; border-radius:1rem; justify-content: center ; align-items: center;" 
+    action="Output.php" 
+    method="POST">
+        
+        <input 
+        style="all:initial; font-family: monospace; font-size: 20px; padding: 1rem 2rem; background-color: lightcyan; margin: 1rem; border-radius: 2rem; color: black; " type="text" 
+        placeholder="enter your name" 
+        name="name"
+        >
+
+        <input 
+        style="all:initial; font-family: monospace; font-size: 20px; padding: 1rem 2rem; background-color: lightcyan; margin: 1rem; border-radius: 2rem; color: black; " 
+        type="text" 
+        placeholder="subject one marks" 
+        name="sub1"
+        >
+
+        <input 
+        style="all:initial; font-family: monospace; font-size: 20px; padding: 1rem 2rem; background-color: lightcyan; margin: 1rem; border-radius: 2rem; color: black; " 
+        type="text" 
+        placeholder="subject two marks"  
+        name="sub2"
+        >
+
+        <input 
+        style="all:initial; font-family: monospace; font-size: 20px; padding: 1rem 2rem; background-color: lightcyan; margin: 1rem; border-radius: 2rem; color: black; " 
+        type="text" 
+        placeholder="subject three marks"  
+        name="sub3"
+        >
+
+        <input 
+        style="all:initial; font-family: monospace; font-size: 20px; padding:0.3rem 1rem; background-color: orangered; border-radius: 3rem; color: white;" 
+        type="submit" 
+        name="submit"
+        >
+    </form>
 </body>
 </html>
-
-<?php
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $subject1 = $_POST["subject1"];
-    $subject2 = $_POST["subject2"];
-    $subject3 = $_POST["subject3"];
-    $total = $subject1 + $subject2 + $subject3;
-    $percentage = ($total / 300) * 100;
-
-    echo "Username: $username <br>";
-    echo "Subject 1: $subject1 <br>";
-    echo "Subject 2: $subject2 <br>";
-    echo "Subject 3: $subject3 <br>";
-    echo "Total: $total <br>";
-    echo "Percentage: $percentage%";
-}
-
-?>
